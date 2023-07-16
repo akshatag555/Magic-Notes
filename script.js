d9d85773f46c0f9ae53eb03e03c52a6e52d7141a
akshatag555 <134508300+akshatag555@users.noreply.github.com>
2023-07-17 02:30:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let cnt = 0;
+allStorage()
+clear.addEventListener("click", (e) => {
+  todo.innerHTML = ""
+}
+)
+submit.addEventListener("click", (e) => {
+  e.preventDefault()
+  let titlec = title.value;
+  let descc = desc.value;
+  localStorage.setItem(cnt++, JSON.stringify([titlec, descc]))
+  title.value = ""
+  desc.value = ""
+  todo.innerHTML = ""
+  allStorage()
+  //added(cnt-1);
+})
+
+function allStorage() {
+
+  var archive = [],
+    keys = Object.keys(localStorage),
+    i = 0, key;
+
+  for (; key = keys[i]; i++) {
+    cnt++
+    //console.log(key)
+    let a = localStorage.getItem(key)
+    let b = JSON.parse(a)
+    for (let i = 0; i < 1; i++) {
+      todo.innerHTML += `
+   <div class="card" style="width: 15rem; margin-left:60px; margin-top:25px"  >
+  <div class="card-body">
+    <h5 class="card-title">${b[i]}</h5>
+    <p class="card-text"> ${b[i + 1]} </p>
+  </div>
+  <button id=${key} class="btn btn-danger"  onclick="del(this.id)" >Delete</button>
+  
+</div>
+
+  `
+    }
+  }
+}
+function added(key) {
+  let a = localStorage.getItem(key)
+  let b = JSON.parse(a)
+  for (let i = 0; i < 1; i++) {
+    todo.innerHTML += `
+   <div class="card" style="width: 15rem; margin-left:60px; margin-top:25px"  >
+  <div class="card-body">
+    <h5 class="card-title">${b[i]}</h5>
+    <p class="card-text"> ${b[i + 1]} </p>
+  </div>
+  <button id="${key}" class="btn btn-danger"  onclick="del(this.id)">Delete</button>
+  
+</div>
+
+  `
+  }
+}
+function del(key) {
+  localStorage.removeItem(key)
+  todo.innerHTML = ""
+  allStorage()
+}
